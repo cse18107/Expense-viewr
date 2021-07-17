30fdf4eeee606c5d5089f0c5ac00c00d714bd617
cse18107 <cse18107@cemk.ac.in>
2021-07-17 06:27:23 +0530

    adding ExpenseItem.js file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
+
+
+{/* <header className="App-header">
+        <h1>Hi there</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +26,4 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */}
